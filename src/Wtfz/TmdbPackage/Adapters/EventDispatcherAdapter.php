cbda6a9b5eb1b056ccb6a434e455d02276f2282e
Mark Redeman <markredeman@gmail.com>
2015-04-02 14:55:52 +0200

    Added EventDispatcherAdapters for Laravel 4 and 5

---
+++ src/Wtfz/TmdbPackage/Adapters/EventDispatcherAdapter.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Wtfz\TmdbPackage\Adapters;
+
+use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface as SymfonyDispatcher;
+
+/**
+ * This adapter provides a Laravel integration for applications
+ * using the Symfony EventDispatcherInterface
+ * It passes any request on to a Symfony Dispatcher and only
+ * uses the Laravel Dispatcher only when dispatching events
+ */
+abstract class EventDispatcherAdapter implements SymfonyDispatcher
+{
+
+    /**
+     * The Laravel Events Dispatcher
+     * @var Illuminate\Contracts\Events\Dispatcher or Illuminate\Events\Dispatcher
+     */
+    protected $laravelDispatcher;
+
+    /**
+     * The Symfony Event Dispatcher
+     * @var  Symfony\Component\EventDispatcher
+     */
+    protected $symfonyDispatcher;
+
+    /**
+     * Dispatches an event to all registered listeners.
+     *
+     * @param string $eventName The name of the event to dispatch. The name of
+     *                          the event is the name of the method that is
+     *                          invoked on listeners.
+     * @param Event  $event     The event to pass to the event handlers/listeners.
+     *                          If not supplied, an empty Event instance is created.
+     *
+     * @return Event
+     *
+     * @api
+     */
+    public function dispatch($eventName, Event $event = null)
+    {
+        if ($event == null)
+        {
+            $event = new Event();
+        }
+
+        $event->setName($eventName);
+        $event->setDispatcher($this);
+
+        $this->laravelDispatcher->fire($eventName, $event);
+        $this->symfonyDispatcher->dispatch($eventName, $event);
+
+        $event->setDispatcher($this);
+
+        return $event;
+    }
+
+    /**
+     * Adds an event listener that listens on the specified events.
+     *
+     * @param string   $eventName The event to listen on
+     * @param callable $listener  The listener
+     * @param int      $priority  The higher this value, the earlier an event
+     *                            listener will be triggered in the chain (defaults to 0)
+     *
+     * @api
+     */
+    public function addListener($eventName, $listener, $priority = 0)
+    {
+        $this->symfonyDispatcher->addListener($eventName, $listener, $priority);
+    }
+
+    /**
+     * Adds an event subscriber.
+     *
+     * The subscriber is asked for all the events he is
+     * interested in and added as a listener for these events.
+     *
+     * @param EventSubscriberInterface $subscriber The subscriber.
+     *
+     * @api
+     */
+    public function addSubscriber(EventSubscriberInterface $subscriber)
+    {
+        $this->symfonyDispatcher->addSubscriber($subscriber);
+    }
+
+    /**
+     * Removes an event listener from the specified events.
+     *
+     * @param string   $eventName The event to remove a listener from
+     * @param callable $listener  The listener to remove
+     */
+    public function removeListener($eventName, $listenerToBeRemoved)
+    {
+        $this->symfonyDispatcher->removeListener($eventName, $listenerToBeRemoved);
+    }
+
+    /**
+     * Removes an event subscriber.
+     *
+     * @param EventSubscriberInterface $subscriber The subscriber
+     */
+    public function removeSubscriber(EventSubscriberInterface $subscriber)
+    {
+        $this->symfonyDispatcher->removeSubscriber($subscriber);
+    }
+
+    /**
+     * Gets the listeners of a specific event or all listeners.
+     *
+     * @param string $eventName The name of the event
+     *
+     * @return array The event listeners for the specified event, or all event listeners by event name
+     */
+    public function getListeners($eventName = null)
+    {
+        return $this->symfonyDispatcher->getListeners($eventName);
+    }
+
+    /**
+     * Checks whether an event has any registered listeners.
+     *
+     * @param string $eventName The name of the event
+     *
+     * @return bool true if the specified event has any listeners, false otherwise
+     */
+    public function hasListeners($eventName = null)
+    {
+        return ($this->symfonyDispatcher->hasListeners($eventName) ||
+            $this->laravelDispatcher->hasListeners($eventName));
+    }
+}
---
+++ src/Wtfz/TmdbPackage/Adapters/Laravel5EventDispatcher.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Wtfz\TmdbPackage\Adapters;
+
+use Symfony\Component\EventDispatcher\EventDispatcherInterface as SymfonyDispatcher;
+use Illuminate\Contracts\Events\Dispatcher as LaravelDispatcher;
+
+/**
+ * This adapter provides a Laravel integration for applications
+ * using the Symfony EventDispatcherInterface
+ * It passes any request on to a Symfony Dispatcher and only
+ * uses the Laravel Dispatcher only when dispatching events
+ */
+class Laravel5EventDispatcher extends EventDispatcherAdapter
+{
+    /**
+     * Forward all methods to the Laravel Events Dispatcher
+     * @param Symfony\Component\EventDispatcher\EventDispatcherInterface $symfonyDispatcher
+     * @param Illuminate\Contracts\Events\Dispatcher $laravelDispatcher
+     */
+    public function __construct(LaravelDispatcher $laravelDispatcher, SymfonyDispatcher $symfonyDispatcher)
+    {
+        $this->laravelDispatcher = $laravelDispatcher;
+        $this->symfonyDispatcher = $symfonyDispatcher;
+    }
+}
